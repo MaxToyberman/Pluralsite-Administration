c4e8399dd3e011cbaab49d078e73cdbd10407b1e
MaxToyberman <maximtoyberman@Maxims-iMac.local>
2016-10-24 07:59:51 +0300

    Add react router

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute=Router.NotFoundRoute;
+
+var routes = (
+
+    <Route name="app" path="/" handler={require('./app')}>
+        <DefaultRoute handler={require('./components/homePage')}/>
+        <Route name="authors" handler={require('./components/authors/authorPage')}/>
+        <Route name="about" handler={require('./components/about/aboutPage')}/>
+        <NotFoundRoute handler={require('./components/NotFoundPage')}/>
+
+    </Route>
+
+);
+
+module.exports = routes;
